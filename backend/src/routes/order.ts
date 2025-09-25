a824fd534c90d5e1cfbb8d23f3a691d1331cd115
Mestr3z <mestr3z@yandex.ru>
2025-09-25 17:17:10 +0300

    fix

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,54 +1,64 @@
-import { Router, type RequestHandler } from 'express'
-import { validateOrderBody } from '../middlewares/validations'
+import { Router, type RequestHandler } from 'express';
+import { validateOrderBody } from '../middlewares/validations';
 import {
-    auth,
-    roleGuardMiddleware,
-    type ReqWithUser,
-} from '../middlewares/auth'
-import { Role } from '../models/user'
+  auth,
+  roleGuardMiddleware,
+  type ReqWithUser,
+} from '../middlewares/auth';
+import { Role } from '../models/user';
 import {
-    getOrders,
-    getOrdersCurrentUser,
-    getOrderByNumber,
-    getOrderCurrentUserByNumber,
-    createOrder,
-    updateOrder,
-    deleteOrder,
-} from '../controllers/order'
+  getOrders,
+  getOrdersCurrentUser,
+  getOrderByNumber,
+  getOrderCurrentUserByNumber,
+  createOrder,
+  updateOrder,
+  deleteOrder,
+} from '../controllers/order';
+import { clampOrdersLimit10 } from '../middlewares/normalizeLimit';
 
-import { clampOrdersLimit10 } from '../middlewares/normalizeLimit'
-
-const router = Router()
+const router = Router();
 
 const withUser =
-    (h: (req: ReqWithUser, ...a: any[]) => any): RequestHandler =>
-    (req, res, next) =>
-        h(req as ReqWithUser, res, next)
+  (h: (req: ReqWithUser, ...a: any[]) => any): RequestHandler =>
+  (req, res, next) =>
+    h(req as ReqWithUser, res, next);
+
 
 router.get(
-    '/',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    clampOrdersLimit10,
-    getOrders
-)
-router.get('/me', auth, withUser(getOrdersCurrentUser))
-router.get('/me/:orderNumber', auth, withUser(getOrderCurrentUserByNumber))
-router.get(
-    '/:orderNumber',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    getOrderByNumber
-)
+  '/all',
+  auth,
+  roleGuardMiddleware(Role.Admin),
+  clampOrdersLimit10,
+  getOrders
+);
 
-router.post('/', validateOrderBody, withUser(createOrder))
+router.get(
+  '/',
+  auth,
+  roleGuardMiddleware(Role.Admin),
+  clampOrdersLimit10,
+  getOrders
+);
+
+router.get('/me', auth, withUser(getOrdersCurrentUser));
+router.get('/me/:orderNumber', auth, withUser(getOrderCurrentUserByNumber));
+
+router.get(
+  '/:orderNumber',
+  auth,
+  roleGuardMiddleware(Role.Admin),
+  getOrderByNumber
+);
+
+router.post('/', validateOrderBody, withUser(createOrder));
 
 router.patch(
-    '/:orderNumber',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    updateOrder
-)
-router.delete('/:id', auth, roleGuardMiddleware(Role.Admin), deleteOrder)
+  '/:orderNumber',
+  auth,
+  roleGuardMiddleware(Role.Admin),
+  updateOrder
+);
+router.delete('/:id', auth, roleGuardMiddleware(Role.Admin), deleteOrder);
 
-export default router
+export default router;
